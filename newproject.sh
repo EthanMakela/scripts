38979401d8acf35222df3ff7bcc723e275348904
Ethan Makela <Ethan.Makela51@gmail.com>
2013-10-24 19:21:41 -0700

    Added script to generate projects

---
+++ newproject.sh
@@ -0,0 +1,67 @@
+echo "New folder name"
+read foldername
+echo "What should the file name be?"
+read filename
+
+
+echo "What type of project do you want to generate?\n
+1) .cpp
+2) .h
+Enter a number"
+read projecttype
+
+if [ $projecttype == 1 ]; then
+	mkdir $foldername
+	cd $foldername
+	touch $filename.cpp
+	mainfile=$filename.cpp
+	echo "
+	//Author: Ethan Makela
+	//Program: $mainfile
+	//$date
+	//Description: 
+	
+	#include <iostream>
+
+	using namespace std;
+
+	int main ()
+	{
+	return 0
+	}" >> $mainfile
+
+
+elif [ $projecttype == 2 ]; then
+	
+	mkdir $foldername
+	cd $foldername
+	touch $filename.h
+	mainfile=$filename.h
+	echo" 
+	//Author: Ethan Makela
+	//Program: $mainfile
+	//$date
+	//Description: 
+	
+	#include <iostream>
+
+	using namespace std;
+	template <class LT> class $foldername;
+	
+	template \<class LT\>
+	class $filename
+	{
+		public:
+
+		private:
+	}
+
+" >> $mainfile
+fi 
+
+
+echo "File name: $mainfile
+Author: $(whoami)
+Date: $(date)" > README.md 
+
+echo DONE
